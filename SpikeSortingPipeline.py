898ce4961e1d6630cb6a17195870bfd0aa9a6735
Lars Werner <lars.werner@stud.uni-due.de>
2021-03-02 17:19:58 +0100

    Comment changes

--- SpikeSortingPipeline.py
+++ SpikeSortingPipeline.py
@@ -478,7 +478,7 @@ def printPerformance(sorter_list):
 def compareSorters(sorter_entry):
 
     # Parameter
-        # sorter_list   : list of recording with its results
+        # sorter_entry   : entry of the recording list with its sorting results
     # No Returns
     
     # Comparing every sorter with each other
@@ -511,7 +511,7 @@ def compareMultipleSorters(sorter_list):
         # sorter_list   : list of recording with its results
     # No Returns
 
-    # Printing Multicomparion graphs
+    # Printing Multicomparion graphs for every recording
     for rec in sorter_list:
         sorters = list()
         for sorter in rec[1]:
